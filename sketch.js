10e3232cacdf062ec8b1a948960e0e6f5f811d40
HeavenlyBornDoley <70126313+HeavenlyBornDoley@users.noreply.github.com>
2020-09-07 10:49:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ weight = random(400, 1500);
 car=createSprite(50, 200, 50, 50);
 
 wall=createSprite(1500, 200, 60, 200);
-wall.shapeColour(80, 80, 80);
+wall.shapeColor(80, 80, 80);
 }
 
 function draw() {
@@ -25,14 +25,14 @@ if(wall.x - car.x < (car.width + wall.width)/2){
   var deformation = 0.5 * width * speed * speed/22509;
 
   if(deformation > 180)
-  {car.shapeColour(255, 0, 0)}
+  {car.shapeColor(255, 0, 0)}
 
 if(deformation<100 && deformation>100){
-  car.shapeColour(230, 230, 0)
+  car.shapeColor(230, 230, 0)
 }
 
 if(deformation<100){
-  car.shapeColour(0, 255, 0)
+  car.shapeColor(0, 255, 0)
 }
 
   drawSprites();
